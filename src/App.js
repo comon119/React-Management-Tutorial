c951bde3605c5066eb0859d046c21d411d1e50f5
Bamgoguma <comon119@nate.com>
2021-10-23 22:31:31 +0900

    Create Cusomer.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [{
+  'id': 1,
+  'image': 'https://placeimg.com/64/64/1',
+  'name': '홍길동',
+  'birthday': '961222',
+  'gender': '남자',
+  'job': '대학생'
+},
+{
+  'id': 2,
+  'image': 'https://placeimg.com/64/64/2',
+  'name': '양승정',
+  'birthday': '721115',
+  'gender': '남자',
+  'job': '개발자'
+},
+{
+  'id': 3,
+  'image': 'https://placeimg.com/64/64/3',
+  'name': '이유일',
+  'birthday': '780102',
+  'gender': '남자',
+  'job': '배달'
+},
+]
 
 function App() {
   return (
-   <div className="gray-background">
-     <img src={logo} lat="logo" />
-     <h2>Let's delvelop management system!</h2>
-   </div>
+    <div>
+      {
+        customers.map(c => {
+          return (
+            <Customer 
+              id={c.id}
+              image={c.image}
+              name={c.name}
+              birthday={c.birthday}
+              gender={c.gender}
+              job={c.job}
+            />
+          );
+        })
+      }
+    </div>
   );
 }
 
